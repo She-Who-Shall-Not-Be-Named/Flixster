f931755be748af4fcfa7b3f41650cb8dc4143451
She-Who-Shall-Not-Be-Named <chanellesactive@gmail.com>
2021-09-16 03:05:54 -0400

    Added movie details, grid cells and launch screen
    Adding MovieDetailsViewController, MovieGridCell, MovieGridController, Updating MoviesViewController and  launch screen

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -22,7 +22,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
-        print("Hello")
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -74,6 +73,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         
         return cell
+        
+        
+    }
+    
+    
+    
+    override func prepare(for segue:
+        UIStoryboardSegue, sender: Any? ) {
+        
+        print("Loading up details screen")
+        
+        // find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
